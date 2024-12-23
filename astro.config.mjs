ff058b76f18b95d668658786fc22975ed0b929db
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-12-23 09:38:53 +0000

    add default embedding

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import embeds from 'astro-embed/integration';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yadobler.github.io/',
-	integrations: [mdx(), sitemap()],
+	integrations: [embeds(), mdx(), sitemap()],
 });
