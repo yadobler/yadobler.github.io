cbfe6ce0df2fad68501b72d7cce63f69e0c10bf7
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-01-04 18:10:53 +0000

    Replace config with consts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,18 @@
-import { defineCollection, z } from 'astro:content';
+import { SITE } from "@consts";
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
+	type: 'content_layer',
+    loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
+        author: z.string().default(SITE.author),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 		hideListing: z.coerce.boolean().optional(),
+		description: z.string(),
 	}),
 });
 
