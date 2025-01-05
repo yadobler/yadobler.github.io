681a6e4274fb7314150ceedc288a3703b50dc2ca
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-01-05 14:30:28 +0000

    Add embeds integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
 import tailwind from '@astrojs/tailwind';
+import embeds from 'astro-embed/integration';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), embeds(), tailwind()]
 });
