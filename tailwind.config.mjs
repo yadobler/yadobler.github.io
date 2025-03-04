5d90898b23c41bb693e908ec465eca999981eb95
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-03-04 21:53:28 +0800

    Add palette into tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,40 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-    ],
-}
+    content: ['./src/**/*.{astro,html,js,ts,jsx,tsx,md,mdx}'],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#1E40AF",         
+                "primary-light": "#93C5FD", 
+                "primary-dark": "#1D4ED8", 
+
+                "secondary": "#9333EA",       
+                "secondary-light": "#D8B4FE",
+                "secondary-dark": "#7E22CE",
+
+                "tertiary": "#34D399",       
+                "tertiary-light": "#A7F3D0",
+                "tertiary-dark": "#10B981",
+            },
+            typography: {
+                DEFAULT: {
+                    css: {
+                        h1: { color: "var(--primary)" },
+                        h2: { color: "var(--secondary)" },
+                        p: { color: "var(--text)" },
+                        a: { color: "var(--link)" },
+                    },
+                },
+                dark: {
+                    css: {
+                        h1: { color: "var(--primary-light)" },
+                        h2: { color: "var(--secondary-light)" },
+                        p: { color: "var(--text-light)" },
+                        a: { color: "var(--link-light)" },
+                    },
+                },
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
+};
