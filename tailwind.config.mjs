2c1d718b9afb188656ccc9c4b435c477050d1236
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-03-05 20:48:27 +0800

    Standardised and improve styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,33 +4,100 @@ export default {
     theme: {
         extend: {
             colors: {
-                "primary": "#1E40AF",         
-                "primary-light": "#93C5FD", 
-                "primary-dark": "#1D4ED8", 
+                
+                "primary-fg-dark": "#dde1e6", 
+                "primary-bg-dark": "#393939", 
+                "primary-fg-light": "#262626",
+                "primary-bg-light": "#90A4AE",
 
-                "secondary": "#9333EA",       
-                "secondary-light": "#D8B4FE",
-                "secondary-dark": "#7E22CE",
+                "secondary-fg-dark": "#ee5396", 
+                "secondary-bg-dark": "#262626", 
+                "secondary-fg-light": "#ee5396",
+                "secondary-bg-light": "#f2f4f8",
 
-                "tertiary": "#34D399",       
-                "tertiary-light": "#A7F3D0",
-                "tertiary-dark": "#10B981",
+                "tertiary-fg-dark": "#08bdba",
+                "tertiary-bg-dark": "#161616", 
+                "tertiary-fg-light": "#08bdba",
+                "tertiary-bg-light": "#dde1e6",
+                
+                "hover-dark": "#be95ff", 
+                "link-dark": "#33b1ff", 
+                "hover-light": "#be95ff",
+                "link-light": "#0f62fe",
             },
             typography: {
                 DEFAULT: {
                     css: {
-                        h1: { color: "var(--primary)" },
-                        h2: { color: "var(--secondary)" },
-                        p: { color: "var(--text)" },
-                        a: { color: "var(--link)" },
+                        h1: { 
+                            color: "var(--secondary-fg-light)", 
+                            fontWeight: "600",
+                            fontSize: "2.25rem",
+                        },
+                        h2: { 
+                            color: "var(--tertiary-fg-light)", 
+                            fontWeight: "500",
+                            fontSize: "1.75rem",
+                        },
+                        h3: { 
+                            color: "var(--tertiary-fg-light)", 
+                            fontWeight: "400",
+                            fontSize: "1.75rem",
+                        },
+                        h4: { 
+                            color: "var(--tertiary-fg-light)", 
+                            fontSize: "1rem",
+                            fontSize: "1.75rem",
+                        },
+                        p: { 
+                            color: "var(--primary-fg-light)", 
+                            fontSize: "1rem",
+                            lineHeight: "1.75rem",
+                        },
+                        a: { 
+                            color: "var(--link-light)", 
+                            textDecoration: "none",
+                            transition: "color 0.3s ease",
+                        },
+                        'a:hover': {
+                            color: "var(--hover-light)",
+                        },
                     },
                 },
                 dark: {
                     css: {
-                        h1: { color: "var(--primary-light)" },
-                        h2: { color: "var(--secondary-light)" },
-                        p: { color: "var(--text-light)" },
-                        a: { color: "var(--link-light)" },
+                        h1: { 
+                            color: "var(--secondary-fg-dark)", 
+                            fontWeight: "600",
+                            fontSize: "2.25rem",
+                        },
+                        h2: { 
+                            color: "var(--tertiary-fg-dark)", 
+                            fontWeight: "500",
+                            fontSize: "1.75rem",
+                        },
+                        h3: { 
+                            color: "var(--tertiary-fg-dark)", 
+                            fontWeight: "400",
+                            fontSize: "1.75rem",
+                        },
+                        h4: { 
+                            color: "var(--tertiary-fg-dark)", 
+                            fontSize: "1rem",
+                            fontSize: "1.75rem",
+                        },
+                        p: { 
+                            color: "var(--primary-fg-dark)", 
+                            fontSize: "1rem",
+                            lineHeight: "1.75rem",
+                        },
+                        a: { 
+                            color: "var(--link-dark)", 
+                            textDecoration: "none",
+                            transition: "color 0.3s ease",
+                        },
+                        'a:hover': {
+                            color: "var(--hover-dark)",
+                        },
                     },
                 },
             },
