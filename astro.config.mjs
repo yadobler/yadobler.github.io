52193bc979cc05464c955d018bd3c4693f6d45bc
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-05-11 03:15:36 +0800

    Add grammars

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import embeds from 'astro-embed/integration';
+import { bundledLanguages } from 'shiki';
+
+import cGrammar from '.grammars/c.json' assert { type: 'json' };
+import nasmGrammar from '.grammars/nasm.json' assert { type: 'json' }; 
+import manPageGrammar from '.grammars/man.json' assert { type: 'json' }; 
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,6 +21,28 @@ export default defineConfig({
                 light: 'github-light',
                 dark: 'github-dark',
             },
+            langs: [
+                ...Object.keys(bundledLanguages), // Keep all bundled languages
+
+                {
+                    id: 'c', // For ```c
+                    scopeName: 'source.c',
+                    grammar: cGrammar,
+                },
+
+                {
+                    id: 'nasm', // For ```nasm
+                    scopeName: 'source.asm.x86_64', 
+                    grammar: nasmGrammar,
+                },
+
+                {
+                    id: 'man', // For ```man
+                    scopeName: 'text.roff', 
+                    grammar: manPageGrammar,
+                },
+
+            ],
         },
     },
     css: {
