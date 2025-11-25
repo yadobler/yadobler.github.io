18f34358710f1755bb05345783703d6705b23120
Yadobler <49450747+yadobler@users.noreply.github.com>
2025-11-26 01:43:10 +0800

    Attempt fix dependency collision

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,53 +6,56 @@ import embeds from 'astro-embed/integration';
 import { bundledLanguages } from 'shiki';
 
 import cGrammar from './.grammars/c.json' assert { type: 'json' };
-import nasmGrammar from './.grammars/nasm.json' assert { type: 'json' }; 
-import manPageGrammar from './.grammars/man.json' assert { type: 'json' }; 
+import nasmGrammar from './.grammars/nasm.json' assert { type: 'json' };
+import manPageGrammar from './.grammars/man.json' assert { type: 'json' };
 
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yadobler.github.io/',
-    trailingSlash: "ignore",
-    integrations: [embeds(), mdx(), tailwind()],
-    markdown: {
-        shikiConfig: {
-            themes: {
-                light: 'github-dark',
-                dark: 'github-dark',
-            },
-            langs: [
-                // @ts-ignore
-                ...Object.keys(bundledLanguages), // Keep all bundled languages
+  site: 'https://yadobler.github.io/',
+  trailingSlash: "ignore",
+  integrations: [embeds(), mdx(), tailwind()],
 
-                {
-                    // @ts-ignore
-                    id: 'c', // For ```c
-                    scopeName: 'source.c',
-                    grammar: cGrammar,
-                },
+  markdown: {
+      shikiConfig: {
+          themes: {
+              light: 'github-dark',
+              dark: 'github-dark',
+          },
+          langs: [
+              // @ts-ignore
+              ...Object.keys(bundledLanguages), // Keep all bundled languages
 
-                {
-                    // @ts-ignore
-                    id: 'nasm', // For ```nasm
-                    scopeName: 'source.asm.x86_64', 
-                    grammar: nasmGrammar,
-                },
+              {
+                  // @ts-ignore
+                  id: 'c', // For ```c
+                  scopeName: 'source.c',
+                  grammar: cGrammar,
+              },
 
-                {
-                    // @ts-ignore
-                    id: 'man', // For ```man
-                    scopeName: 'text.roff', 
-                    grammar: manPageGrammar,
-                },
+              {
+                  // @ts-ignore
+                  id: 'nasm', // For ```nasm
+                  scopeName: 'source.asm.x86_64', 
+                  grammar: nasmGrammar,
+              },
 
-            ],
-        },
-    },
-    css: {
-        global: true,
-    },
-    experimental: {
-        // svg: true,
-    }
+              {
+                  // @ts-ignore
+                  id: 'man', // For ```man
+                  scopeName: 'text.roff', 
+                  grammar: manPageGrammar,
+              },
+
+          ],
+      },
+  },
+
+  css: {
+      global: true,
+  },
+
+  experimental: {
+      // svg: true,
+  },
 });
