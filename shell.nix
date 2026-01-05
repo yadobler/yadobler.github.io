8d326d5c18214c55f3205d08aa461d441bb93c12
Yadobler <49450747+yadobler@users.noreply.github.com>
2026-01-05 19:07:46 +0800

    Migrate to pnpm

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   packages = with pkgs; [
-    nodejs_24
-    npm-check-updates
+    pnpm
+    # nodejs_24
+    # npm-check-updates
     # bootstrap-studio
   ];
 
