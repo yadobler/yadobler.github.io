84deca5aaa4decbb5057377db37bd865014bd460
Yadobler <49450747+yadobler@users.noreply.github.com>
2026-04-14 08:17:22 +0800

    Fix shiki issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,50 +12,50 @@ import manPageGrammar from './.grammars/man.json' assert { type: 'json' };
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yadobler.github.io/',
-  trailingSlash: "ignore",
-  integrations: [embeds(), mdx(), tailwind()],
+    site: 'https://yadobler.github.io/',
+    trailingSlash: "ignore",
+    integrations: [embeds(), mdx(), tailwind()],
 
-  markdown: {
-      shikiConfig: {
-          themes: {
-              light: 'github-dark',
-              dark: 'github-dark',
-          },
-          langs: [
-              // @ts-ignore
-              ...Object.keys(bundledLanguages), // Keep all bundled languages
+    markdown: {
+        shikiConfig: {
+            themes: {
+                light: 'github-dark',
+                dark: 'github-dark',
+            },
+            langs: [
+                // @ts-ignore
+                // Keep all bundled languages
+                ...Object.keys(bundledLanguages), 
 
-              {
-                  // @ts-ignore
-                  id: 'c', // For ```c
-                  scopeName: 'source.c',
-                  grammar: cGrammar,
-              },
+                // @ts-ignore
+                {
+                    ...cGrammar, 
+                    name: 'c', 
+                    scopeName: 'source.c',
+                },
 
-              {
-                  // @ts-ignore
-                  id: 'nasm', // For ```nasm
-                  scopeName: 'source.asm.x86_64', 
-                  grammar: nasmGrammar,
-              },
+                // @ts-ignore
+                {
+                    ...nasmGrammar,
+                    name: 'nasm',
+                    scopeName: 'source.asm.x86_64', 
+                },
 
-              {
-                  // @ts-ignore
-                  id: 'man', // For ```man
-                  scopeName: 'text.roff', 
-                  grammar: manPageGrammar,
-              },
+                // @ts-ignore
+                {
+                    ...manPageGrammar,
+                    name: 'man',
+                    scopeName: 'text.roff', 
+                },
+            ],
+        },
+    },
 
-          ],
-      },
-  },
+    css: {
+        global: true,
+    },
 
-  css: {
-      global: true,
-  },
-
-  experimental: {
-      // svg: true,
-  },
+    experimental: {
+        // svg: true,
+    },
 });
